e1ca601fbeccb891ab63aa0c21697d2744409415
Jacky <newbdez33@gmail.com>
2017-06-03 19:04:37 +0800

    fix missing price

--- test.php
+++ test.php
@@ -5,6 +5,6 @@ require "./common.inc.php";
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
-$url = "https://www.amazon.co.jp/dp/B016MO9AR6/";
+$url = "https://www.amazon.co.jp/dp/B00H2EIU86/ref=cm_sw_r_oth_api_fRAgzbJ61VWND?th=1&psc=1";
 $fetched = fetchAmazonUrl($url);
 print_r($fetched);
