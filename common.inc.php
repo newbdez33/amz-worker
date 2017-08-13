e40e82f782965f08cd4509942ea444493b8ebfb6
Jacky <newbdez33@gmail.com>
2017-08-14 00:38:03 +0900

    try fix price issue

--- common.inc.php
+++ common.inc.php
@@ -273,6 +273,28 @@ function fetchAmazonUrl($url) {
 
 	if ($data["price"] == '0') {
 		try {
+			$element = $webDriver->findElement(WebDriverBy::id("tmmSwatches"));
+			$element = $element->findElement(WebDriverBy::className("a-color-price"));
+			if ($element->isDisplayed()) {
+	    		$price = $element->getText();
+	    		echo "price2:{$price}\n";
+				if ($price != "") {
+					$result = convertPrice($price, $url);
+					$data["price"] = doubleval($result["price"]);
+					$data["currency"] = $result["currency"];
+				}
+			}
+		} catch(Exception $e) {
+			//$log->debug("find element price failed.");
+			//print_r($e);
+			//TODO send alert mail.
+		} finally {
+			//
+		}
+	}
+
+	if ($data["price"] == '0') {
+		try {
 			$element = $webDriver->findElement(WebDriverBy::id("snsPrice"));
 			$element = $element->findElement(WebDriverBy::className("a-color-price"));
 			if ($element->isDisplayed()) {
